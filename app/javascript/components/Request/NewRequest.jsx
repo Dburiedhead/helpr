e34d35fbc9a18ec33152237cbbebee6d7cb9aa8f
Dburiedhead <dmsoumare@sooja.fr>
2021-06-22 16:16:17 +0200

    Fixed request form state emptying bug

--- app/javascript/components/Request/NewRequest.jsx
+++ app/javascript/components/Request/NewRequest.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import MapGL, {NavigationControl, GeolocateControl} from "react-map-gl";
+import MapGL, { NavigationControl, GeolocateControl } from "react-map-gl";
 import Geocoder from "react-map-gl-geocoder";
-import RequestForm from './RequestForm'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Label } from 'semantic-ui-react';
+import { Form } from 'formsy-semantic-ui-react';
+import axios from 'axios'
+import setAxiosHeaders from '../AxiosHeaders';
 
-const MAPBOX_TOKEN ='pk.eyJ1IjoiZHNvb2phIiwiYSI6ImNra3dob2dwYjFkbzkycG1zNG55NnZzd2IifQ.ZCGuAjiuGCyjlezqMyn5VA'
+
+const selectOptions = [
+  { value: 'task', text: 'Task' },
+  { value: 'material', text: 'Material' },
+];
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiZHNvb2phIiwiYSI6ImNra3dob2dwYjFkbzkycG1zNG55NnZzd2IifQ.ZCGuAjiuGCyjlezqMyn5VA'
 
 const style = {
-    width: '90%',
-    height: '80vh',
+  width: '90%',
+  height: '80vh',
 };
 
 const geolocateStyle = {
@@ -19,82 +27,191 @@ const geolocateStyle = {
   margin: 10
 };
 
-class NewRequest extends Component {
-    state = {
-        viewport: {
-            latitude: 48.856614,
-            longitude: 2.3522219,
-            zoom: 15
-        },
-        requestLat: null,
-        requestLong: null
-      };
-    
-      mapRef = React.createRef();
-      ContainerRef = React.createRef();
+export default class NewRequest extends Component {
+  state = {
+    viewport: {
+      latitude: 48.856614,
+      longitude: 2.3522219,
+      zoom: 15
+    },
+    requestLat: null,
+    requestLong: null,
+    description: '',
+    title: '',
+    request_type: '',
+    latitude: null,
+    longitde: null
+  };
 
-      _onViewportChange = viewport => {
-        this.setState({
-          viewport: { ...this.state.viewport, ...viewport }
-        });
-      };
+  mapRef = React.createRef();
+  ContainerRef = React.createRef();
 
-      _handleResult = result => {
-        this.setState({
-          currMarker: {
-            name: result.result.place_name,
-            latitude: result.result.center[1],
-            longitude: result.result.center[0]
-          }
-        })
-        console.log(result.result.place_name, result.result.center[1], result.result.center[0]);
-        let requestLat = result.result.center[1]
-        let requestLong = result.result.center[0]
-        console.log(requestLat, requestLong)
-        this.setState({requestLat, requestLong})
+  _onViewportChange = viewport => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    });
+  };
+
+  _handleResult = result => {
+    this.setState({
+      currMarker: {
+        name: result.result.place_name,
+        latitude: result.result.center[1],
+        longitude: result.result.center[0]
       }
-    
-      render() {
-        return (
-          <Row style={{ margin: "1em auto" }}>
-            <Col>
-              <div ref={this.containerRef}>
-                  <MapGL
-                      ref={this.mapRef}
-                      { ...style }
-                      style={{ margin: '2em auto' }}
-                      {...this.state.viewport}
-                      onViewportChange={this._onViewportChange}
-                      mapboxApiAccessToken={MAPBOX_TOKEN}
-                  >
-                    <GeolocateControl
-                      style={geolocateStyle}
-                      // positionOptions={positionOptions}
-                      trackUserLocation
-                      auto
-                  />
-                      <NavigationControl
-                      className={'NavigationControl'}
-                      onViewportChange={viewport => this._onViewportChange(viewport)}
-                      />
-                      <Geocoder
-                        mapRef={this.mapRef}
-                        mapboxApiAccessToken={MAPBOX_TOKEN}
-                        position='top-right'
-                        containerRef={this.containerRef}
-                        onResult={this._handleResult}
-                        onViewportChange={this._onViewportChange}
-                        marker={true}
+    })
+    let latitude = result.result.center[1]
+    let longitude = result.result.center[0]
+    this.setState({ latitude, longitude })
+  }
 
-                      />
-                  </MapGL>
-              </div>
-            </Col>
-            {/* { this.state.requestLat && this.state.requestLog && <RequestForm latitude={this.state.requestLat} longitude={this.state.requestLong}/> } */}
-            <RequestForm key={ this.state.requestLat } latitude={this.state.requestLat} longitude={this.state.requestLong}/>
-          </Row>
-        );
+  handleChange = (e, { name, value }) => this.setState({ [name]: value });
+
+  handleSubmit = () => {
+    setAxiosHeaders();
+    const data = {
+      description: this.state.description,
+      title: this.state.title,
+      request_type: this.state.request_type,
+      latitude: this.state.latitude,
+      longitude: this.state.longitude
+    };
+    axios.post('api/v1/requests', data)
+      .then(res => {
+        if (res.request.status === 201) {
+          alert('Request sent ! Thank you for your participation');
+          this.areaForm.reset();
+        }
       }
-    }
-  
-export default NewRequest;
+      )
+      .catch(err => console.log(err))
+  };
+
+  render() {
+    const errorLabel = <Label color="red" pointing />
+
+    return (
+      <Row style={{ margin: "1em auto" }}>
+        <Col>
+          <div ref={this.containerRef}>
+            <MapGL
+              ref={this.mapRef}
+              {...style}
+              style={{ margin: '2em auto' }}
+              {...this.state.viewport}
+              onViewportChange={this._onViewportChange}
+              mapboxApiAccessToken={MAPBOX_TOKEN}
+            >
+              <GeolocateControl
+                style={geolocateStyle}
+                // positionOptions={positionOptions}
+                trackUserLocation
+                auto
+              />
+              <NavigationControl
+                className={'NavigationControl'}
+                onViewportChange={viewport => this._onViewportChange(viewport)}
+              />
+              <Geocoder
+                mapRef={this.mapRef}
+                mapboxApiAccessToken={MAPBOX_TOKEN}
+                position='top-right'
+                containerRef={this.containerRef}
+                onResult={this._handleResult}
+                onViewportChange={this._onViewportChange}
+                marker={true}
+
+              />
+            </MapGL>
+          </div>
+        </Col>
+        {/* { this.state.requestLat && this.state.requestLog && <RequestForm latitude={this.state.requestLat} longitude={this.state.requestLong}/> } */}
+        {/* <RequestForm key={ this.state.requestLat } latitude={this.state.requestLat} longitude={this.state.requestLong}/> */}
+        <Col>
+          <Form
+            // style={{ padding: '2vh 10vw' }}
+            // onSubmit={this.handleSubmit}
+            className="custom-classname-is-rendered"
+            ref={(event) => { this.areaForm = event; }}
+            onValidSubmit={this.handleSubmit}
+          >
+            <Form.Group>
+              <Form.Input
+                name='title'
+                width={10}
+                label='Title'
+                placeholder='Doe'
+                required
+                type="text"
+                value={this.state.title}
+                onChange={this.handleChange}
+                validations="minLength:5"
+                errorLabel={errorLabel}
+                validationErrors={{ minLength: 'Please enter your request title' }}
+
+              />
+              <Form.Dropdown
+                name="request_type"
+                width={2}
+                label="Type"
+                placeholder='Please select…'
+                help="This is required"
+                options={selectOptions}
+                required
+                selection
+                value={this.state.request_type}
+                onChange={this.handleChange}
+                errorLabel={errorLabel}
+                validationError='You need to select a type'
+
+              />
+
+            </Form.Group>
+            <Form.TextArea
+              name="description"
+              label="Description"
+              placeholder="I need ..."
+              help="Max 300 characters."
+              validations="maxLength:300"
+              required
+              validationErrors={{
+                maxLength: 'Please provide less than 300 characters.',
+              }}
+              errorLabel={errorLabel}
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+            <Form.Group>
+              <Form.Input
+                name='latitude'
+                label='Latitude'
+                width={5}
+                // disabled
+                // hidden
+                required
+                value={this.state.latitude}
+                onChange={this.handleChange}
+                errorLabel={errorLabel}
+                validationError='You need to select an adress on the map to set these field'
+              />
+              <Form.Input
+                name='longitude'
+                width={5}
+                label='Longitude'
+                required
+                // disabled
+                // hidden
+                value={this.state.longitude}
+                onChange={this.handleChange}
+                errorLabel={errorLabel}
+                validationError='You need to select an adress on the map to set these field'
+              />
+
+            </Form.Group>
+            <Form.Button className='submit-btn'>Submit</Form.Button>
+          </Form>
+        </Col>
+      </Row>
+    );
+  }
+}
