2b607f38e197dfd6e80ba97486944e57f2a71e4b
Dburiedhead <dmsoumare@sooja.fr>
2021-07-01 03:57:56 +0200

    Back to Minitest

--- config/routes.rb
+++ config/routes.rb
@@ -4,27 +4,27 @@ Rails.application.routes.draw do
     root 'pages#index', as: :authenticated_root
   end
   
-  devise_scope :user do
-    # root to: 'devise/sessions#new'
-    root to: 'pages#index'
-  end
+  # devise_scope :user do
+  #   # root to: 'devise/sessions#new'
+  #   root to: 'pages#index'
+  # end
+
+  root 'pages#index'
   
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :requests, param: :id
-      resources :users, only: %i[show]
-      # get 'is_signed_in', to: 'requests#is_signed_in'
-      get 'current_user_requests', to: 'requests#get_requests_by_user_id'
-      get 'current_user_responses', to: 'conversations#get_responses_by_user_id'
-      # get 'requests_conversations_by_user_id', to 'conversations#get_request_conversations_by_user_id'
-      get 'current_user_first_name', to: 'messages#get_user_first_name'
-      get 'current_user_first_name', to: 'messages#get_user_first_name'
-      get 'current_user_avatar', to: 'conversations#get_user_avatar'
-      get 'current_user_full_name', to: 'conversations#get_user_full_name'
-      get 'unfulfilled_request', to: 'request#unfulfilled_request'
-      resources :conversations, only: [:index, :show, :create, :destroy], param: :id
+      resources :users, param: :id
+      resources :conversations, only: [:index, :show, :create, :destroy, :update], param: :id
       resources :messages, only: [:index, :show, :create]
       mount ActionCable.server => '/cable'
+      get 'get_user_data', to: 'users#get_user_data'
+      get 'current_user_requests', to: 'requests#get_requests_by_user_id'
+      get 'current_user_responses', to: 'conversations#get_responses_by_user_id'
+      get 'current_user_help', to: 'conversations#get_responses_help'
+      # get 'requests_conversations_by_user_id', to 'conversations#get_request_conversations_by_user_id'
+      # get 'current_user_first_name', to: 'messages#get_user_first_name'
+      get 'unfulfilled_request', to: 'request#unfulfilled_request'
     end
   end
   get '*path', to: 'pages#index', via: :all
--- test/controllers/api/v1/conversations_controller_test.rb
+++ test/controllers/api/v1/conversations_controller_test.rb
@@ -4,4 +4,36 @@ class Api::V1::ConversationsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  # index
+  test "should get index as json" do
+    get :index
+    assert_equal "application/json", response.content_type include()
+  end
+
+  test "should return success" do
+    assert_response :success
+    get :index
+  end
+
+  # show
+  test "should return conversation" do
+    conversation = conversations(:one)
+    get conversation_url(conversation)
+    assert_response :success
+  end
+  
+  test "should show conversation that includes current user" do
+    get :show
+    assert_equal current_user.id, conversation.helpr_id || conversation.requester_id
+  end
+  
+  # create
+  test "should create conversation" do
+    assert_difference('Conversation.count') do
+      post '/api/v1/conversations', params: { conversation: { helpr_id: dianka.id, requester_id: juliette.id, title: requests(:one).title, selected: false } }, as: :json
+    end
+    
+    assert_response :suces
+  end
 end
