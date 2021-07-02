f4f9854ae81d0f4fc6978215fdeea29ccaf7628a
Dburiedhead <dmsoumare@sooja.fr>
2021-07-02 16:46:57 +0200

    added semantic UI css to application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
 
     protected
         def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :avatar, :gov_approved_id)}
-            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password, :password_confirmation, :avatar, :gov_approved_id)}
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :gov_approved_id)}
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password, :password_confirmation, :gov_approved_id)}
         end
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,37 +2,37 @@ class User < ApplicationRecord
   has_many :requests
   has_many :conversations, through: :messages
   has_many :messages
-  has_one_attached :avatar
+  # has_one_attached :avatar
   has_one_attached :gov_approved_id
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  after_commit :add_default_avatar, on: %i[create update]
+  # after_commit :add_default_avatar, on: %i[create update]
 
   
-  def avatar_thumbnail
-    if avatar.attached?
-      avatar.variant(resize: "150x150!").processed
-    else
-      '/default_avatar.png'
-    end
-  end
+  # def avatar_thumbnail
+  #   if avatar.attached?
+  #     avatar.variant(resize: "150x150!").processed
+  #   else
+  #     '/default_avatar.png'
+  #   end
+  # end
 
-  private
+  # private
   
-  def add_default_avatar
-    unless avatar.attached?
-      avatar.attach(
-        io: File.open(
-          Rails.root.join(
-            'app', 'assets', 'images', 'default_avatar.png'
-          )
-        ),
-        filename: 'default_avatar.png',
-        content_type: 'image/png, image/jpg'
-      )
-    end
-  end
+  # def add_default_avatar
+  #   unless avatar.attached?
+  #     avatar.attach(
+  #       io: File.open(
+  #         Rails.root.join(
+  #           'app', 'assets', 'images', 'default_avatar.png'
+  #         )
+  #       ),
+  #       filename: 'default_avatar.png',
+  #       content_type: 'image/png, image/jpg'
+  #     )
+  #   end
+  # end
 end
